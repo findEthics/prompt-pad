48a0713ada070c7aec649cb6ae90735f1197da8b
findEthics <finds_ethics.0j@icloud.com>
2026-08-23 12:23:19 +0200

    Reset launcher drawer on Home

--- app/src/main/java/me/pompel/elauncher/MainActivity.java
+++ app/src/main/java/me/pompel/elauncher/MainActivity.java
@@ -179,6 +179,12 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        handleBack();
+    }
+
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ThemePreference.apply(this);
